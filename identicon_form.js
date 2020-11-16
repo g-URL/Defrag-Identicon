7142edcb85a80e316eac8ec14f2f8b5cbe210403
g-URL <rapperley@ucalgary.ca>
2020-11-16 16:42:47 -0700

    Update identicon_form.js

--- identicon_form.js
+++ identicon_form.js
@@ -15,19 +15,19 @@ class IdenticonForm extends React.Component {
 
         // icons
         this.bad = new Image(64,64);
-        this.bad.src = '/assets/icons/bad.png';
+        this.bad.src = './assets/icons/bad.png';
 
         this.beginning = new Image(64, 64);
-        this.beginning.src = '/assets/icons/beginning.png';
+        this.beginning.src = './assets/icons/beginning.png';
 
         this.end = new Image(64, 64);
-        this.end.src = '/assets/icons/end.png';
+        this.end.src = './assets/icons/end.png';
 
         this.middle = new Image(64, 64);
-        this.middle.src = '/assets/icons/middle.png';        
+        this.middle.src = './assets/icons/middle.png';        
 
         this.move = new Image(64, 64);
-        this.move.src = '/assets/icons/move.png';
+        this.move.src = './assets/icons/move.png';
 
         this.optimized = new Image(64, 64);
         this.optimized.src = '/assets/icons/optimized.png';
