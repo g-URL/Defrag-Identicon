25c066a92123519b77869c7f8baf037e6d64666e
g-URL <rapperley@ucalgary.ca>
2020-11-16 14:43:51 -0700

    fetches identicon only when button pressed

--- identicon_form.js
+++ identicon_form.js
@@ -1,42 +1,42 @@
 // https://github.blog/2013-08-14-identicons/
 // fetches identicon based on form input
 
+// IdenticonForm class based off of react tutorial
+// https://reactjs.org/docs/forms.html
+// https://www.sitepoint.com/work-with-forms-in-react/
 class IdenticonForm extends React.Component {
-    // https://reactjs.org/docs/forms.html
     constructor(props) {
         super(props);
 
         this.state = { 
-            value: 'Please enter a valid GitHub handle.'
+            handle: '',
+            url: '',
         };
-
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
-        this.setState({ value: event.target.value })
+        this.setState({ handle: event.target.value });
     }
 
     handleSubmit(event) {
-
-
-        alert('A submission was detected: ' + this.state.value);
         event.preventDefault();
+        console.log(this.state.handle);
+        this.setState({ url: 'https://github.com/identicons/'+this.state.handle+'.png' })
     }
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Handle:
-                    <input type='text' value={this.state.value} onChange={this.handleChange} />
-                    <img
-                        src={'https://github.com/identicons/'+this.state.value+'.png'}
-                    />
-                </label>
-                <input type='submit' value="submit" />
-            </form>
+            <React.Fragment>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor='handle'>Enter GitHub Handle:</label>
+                    <br></br>
+                    <input type='text' value={this.state.handle} onChange={this.handleChange} />
+                    <button type='submit'>Fetch Identicon!</button>    
+                </form>
+                <img src={this.state.url}/>
+            </React.Fragment>
         );
     }
 }
