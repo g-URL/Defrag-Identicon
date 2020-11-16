788b506d8567d9a2ff06d4855285ee331ec49fb1
g-URL <rapperley@ucalgary.ca>
2020-11-16 16:49:50 -0700

    Update identicon_form.js

--- identicon_form.js
+++ identicon_form.js
@@ -14,29 +14,29 @@ class IdenticonForm extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
 
         // icons
-        this.bad = new Image(64,64);
-        this.bad.src = './assets/icons/bad.png';
+        // this.bad = new Image(64,64);
+        // this.bad.src = './assets/icons/bad.png';
 
-        this.beginning = new Image(64, 64);
-        this.beginning.src = './assets/icons/beginning.png';
+        // this.beginning = new Image(64, 64);
+        // this.beginning.src = './assets/icons/beginning.png';
 
-        this.end = new Image(64, 64);
-        this.end.src = './assets/icons/end.png';
+        // this.end = new Image(64, 64);
+        // this.end.src = './assets/icons/end.png';
 
-        this.middle = new Image(64, 64);
-        this.middle.src = './assets/icons/middle.png';        
+        // this.middle = new Image(64, 64);
+        // this.middle.src = './assets/icons/middle.png';        
 
-        this.move = new Image(64, 64);
-        this.move.src = './assets/icons/move.png';
+        // this.move = new Image(64, 64);
+        // this.move.src = './assets/icons/move.png';
 
-        this.optimized = new Image(64, 64);
-        this.optimized.src = '/assets/icons/optimized.png';
+        // this.optimized = new Image(64, 64);
+        // this.optimized.src = '/assets/icons/optimized.png';
 
-        this.read = new Image(64, 64);
-        this.read.src = '/assets/icons/read.png';        
+        // this.read = new Image(64, 64);
+        // this.read.src = '/assets/icons/read.png';        
 
-        this.write = new Image(64, 64);
-        this.write.src = '/assets/icons/write.png';
+        // this.write = new Image(64, 64);
+        // this.write.src = '/assets/icons/write.png';
     }
 
     handleChange(event) {
@@ -60,11 +60,11 @@ class IdenticonForm extends React.Component {
 
         
         // this works!!!
-        context.drawImage(this.optimized, 0, 0);   // looks like coordinates reflect upper-left-hand corner
-        context.drawImage(this.optimized, 220, 220);   // looks like coordinates reflect upper-left-hand corner
+       // context.drawImage(this.optimized, 0, 0);   // looks like coordinates reflect upper-left-hand corner
+       // context.drawImage(this.optimized, 220, 220);   // looks like coordinates reflect upper-left-hand corner
 
-        let pixelData = context.getImageData(1,1,1,1);
-        console.log(pixelData);
+     //   let pixelData = context.getImageData(1,1,1,1);
+     //   console.log(pixelData);
     }
 
     render() {
