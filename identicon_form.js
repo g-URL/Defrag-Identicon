4633bdcc8305a6174946528db7f0482cb85c9445
g-URL <rapperley@ucalgary.ca>
2020-11-16 17:14:36 -0700

    problems with tainted canvas and crossorigin
    need to think of another way to do this

--- identicon_form.js
+++ identicon_form.js
@@ -14,29 +14,29 @@ class IdenticonForm extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
 
         // icons
-        // this.bad = new Image(64,64);
-        // this.bad.src = './assets/icons/bad.png';
+        this.bad = new Image(64,64);
+        this.bad.src = './assets/icons/bad.png';
 
-        // this.beginning = new Image(64, 64);
-        // this.beginning.src = './assets/icons/beginning.png';
+        this.beginning = new Image(64, 64);
+        this.beginning.src = './assets/icons/beginning.png';
 
-        // this.end = new Image(64, 64);
-        // this.end.src = './assets/icons/end.png';
+        this.end = new Image(64, 64);
+        this.end.src = './assets/icons/end.png';
 
-        // this.middle = new Image(64, 64);
-        // this.middle.src = './assets/icons/middle.png';        
+        this.middle = new Image(64, 64);
+        this.middle.src = './assets/icons/middle.png';        
 
-        // this.move = new Image(64, 64);
-        // this.move.src = './assets/icons/move.png';
+        this.move = new Image(64, 64);
+        this.move.src = './assets/icons/move.png';
 
-        // this.optimized = new Image(64, 64);
-        // this.optimized.src = '/assets/icons/optimized.png';
+        this.optimized = new Image(64, 64);
+        this.optimized.src = './assets/icons/optimized.png';
 
-        // this.read = new Image(64, 64);
-        // this.read.src = '/assets/icons/read.png';        
+        this.read = new Image(64, 64);
+        this.read.src = './assets/icons/read.png';        
 
-        // this.write = new Image(64, 64);
-        // this.write.src = '/assets/icons/write.png';
+        this.write = new Image(64, 64);
+        this.write.src = './assets/icons/write.png';
     }
 
     handleChange(event) {
@@ -55,16 +55,13 @@ class IdenticonForm extends React.Component {
         let img = document.getElementById('identicon');
         img.src = this.state.url;
         context.drawImage(img, 0, 0);   // looks like coordinates reflect upper-left-hand corner
-
-
-
         
-        // this works!!!
-       // context.drawImage(this.optimized, 0, 0);   // looks like coordinates reflect upper-left-hand corner
-       // context.drawImage(this.optimized, 220, 220);   // looks like coordinates reflect upper-left-hand corner
+        // CANNOT GET 3RD PARTY PIXEL INFORMATION FROM TAINTED CANVAS - FIND ANOTHER WAY
+        //let pixelData = context.getImageData(1,1,1,1);
+        //console.log(pixelData);
 
-        let pixelData = context.getImageData(1,1,1,1);
-        console.log(pixelData);
+        context.drawImage(this.read, 0, 0);
+
     }
 
     render() {
@@ -76,7 +73,7 @@ class IdenticonForm extends React.Component {
                     <input type='text' value={this.state.handle} onChange={this.handleChange} />
                     <button type='submit'>Fetch Identicon!</button>    
                 </form>
-                <img id='identicon' width='420' height='420' crossorigin src ={this.state.url}/>
+                <img id='identicon' width='420' height='420' src ={this.state.url} />
                 <br></br>
                 <canvas id='myCanvas' height='420' width='420'>
                 </canvas>
