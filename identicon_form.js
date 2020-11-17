8799e66b0cc2b57cbfac7ee060b7458b6e356940
g-URL <rapperley@ucalgary.ca>
2020-11-16 22:34:30 -0700

    now works. will need refactoring

--- identicon_form.js
+++ identicon_form.js
@@ -17,6 +17,8 @@ class IdenticonForm extends React.Component {
 
         this.original = new Image(420,420);
 
+        // CAN WE DO THIS WITH A SPRITESHEET?
+
         // icons
         this.bad = new Image(64,64);
         this.bad.src = './assets/icons/bad.png';
@@ -57,7 +59,7 @@ class IdenticonForm extends React.Component {
             // https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_canvas_drawimage
             const canvas = document.getElementById('myCanvas');
             const context = canvas.getContext('2d');
-            
+
             canvas.style = 'background: url(https://github.com/identicons/'+this.state.handle+'.png)';
 
             context.font = '32px Arial';
@@ -91,25 +93,126 @@ class IdenticonForm extends React.Component {
 
         const canvas2 = document.getElementById('myCanvas2');
         const context2 = canvas2.getContext('2d');
+        context2.clearRect(0, 0, 420, 420);
 
         canvas2.style = 'background-color: rgb(240, 240, 240)';
 
-        let length = this.state.identiconLetters.length;
+        const length = this.state.identiconLetters.length;
+        console.log(length);
 
-        for (let i = 0; i < length; i++) {
-            const letterPosition = this.state.identiconLetters[i].charCodeAt()-'A'.charCodeAt();
-            const rowPosition = (letterPosition/5) >> 0;
-            const columnPosition = letterPosition - 5*rowPosition;
-            context2.drawImage(this.optimized, 38 + 68*columnPosition, 38 + 68*rowPosition);
-        }
+        let writeLocation = (Math.random()*length) >> 0;    // [0,13)
+        let readLocation = (Math.random()*(length-1-writeLocation) >> 0) + writeLocation;   // [0,13]
 
-        let letter = 'A';
-        for (let i = 0; i < 5; i++) {
-            for (let j = 0; j < 5; j++) {
-                context2.fillText(letter, (70 + (j*70)) - 12, 70 + (70*i) + 12);
-                letter = String.fromCharCode(letter.charCodeAt() + 1);
-                
+        let badLocation = (Math.random()*writeLocation) >> 0;       // [0, writeLocation)
+        let toBeMovedLocation = (Math.random()*writeLocation) >> 0; // [0, writeLocation)
+
+        let badLocation2 = (Math.random()*(length-1-writeLocation) >> 0) + writeLocation;   // [0,13]
+        let toBeMovedLocation2 = (Math.random()*(length-1-writeLocation) >> 0) + writeLocation;   // [0,13]
+
+        let middleLocation = (Math.random()*(length-1-readLocation) >> 0) + readLocation;   // [0,13]
+        let endLocation = (Math.random()*(length-1-readLocation) >> 0) + readLocation;   // [0,13]
+
+        console.log(writeLocation, readLocation, badLocation, toBeMovedLocation);
+
+        let writePosition = this.state.identiconLetters[writeLocation].charCodeAt()-'A'.charCodeAt();
+        let readPosition = this.state.identiconLetters[readLocation].charCodeAt()-'A'.charCodeAt();
+        let badPosition = this.state.identiconLetters[badLocation].charCodeAt()-'A'.charCodeAt();
+        let toBeMovedPosition = this.state.identiconLetters[toBeMovedLocation].charCodeAt()-'A'.charCodeAt();
+        let badPosition2 = this.state.identiconLetters[badLocation2].charCodeAt()-'A'.charCodeAt();
+        let toBeMovedPosition2 = this.state.identiconLetters[toBeMovedLocation2].charCodeAt()-'A'.charCodeAt();
+        let middlePosition = this.state.identiconLetters[middleLocation].charCodeAt()-'A'.charCodeAt();
+        let endPosition = this.state.identiconLetters[endLocation].charCodeAt()-'A'.charCodeAt();
+
+
+        console.log(writePosition, readPosition, badPosition, toBeMovedPosition);
+
+        let letterIndex = 0;
+        let letterPosition = this.state.identiconLetters[letterIndex].charCodeAt()-'A'.charCodeAt();
+        for (let i = 0; i < 25; i++) {
+            const rowPosition = (i/5) >> 0;
+            const columnPosition = i - 5*rowPosition;
+
+            if (i == letterPosition) {
+
+                if (i < writePosition) {
+                    if (badPosition < toBeMovedPosition) {
+                        if (i <= badPosition) {
+                            context2.drawImage(this.bad, 38 + 68*columnPosition, 38 + 68*rowPosition);
+                        } else {
+                            context2.drawImage(this.move, 38 + 68*columnPosition, 38 + 68*rowPosition);
+                        }
+                    } else {
+                        if (i <= toBeMovedPosition) {
+                            context2.drawImage(this.move, 38 + 68*columnPosition, 38 + 68*rowPosition);
+                        } else {
+                            context2.drawImage(this.bad, 38 + 68*columnPosition, 38 + 68*rowPosition);
+                        }                    
+                    }
+                }
+    
+                if (i == writePosition) {
+                    context2.drawImage(this.write, 38 + 68*columnPosition, 38 + 68*rowPosition);
+                }
+
+
+                if (i > writePosition) {
+                    if (badPosition2 < toBeMovedPosition2) {
+                        if (i <= badPosition2) {
+                            context2.drawImage(this.bad, 38 + 68*columnPosition, 38 + 68*rowPosition);
+                        } else {
+                            context2.drawImage(this.move, 38 + 68*columnPosition, 38 + 68*rowPosition);
+                        }
+                    } else {
+                        if (i <= toBeMovedPosition2) {
+                            context2.drawImage(this.move, 38 + 68*columnPosition, 38 + 68*rowPosition);
+                        } else {
+                            context2.drawImage(this.bad, 38 + 68*columnPosition, 38 + 68*rowPosition);
+                        }                    
+                    }
+                }
+
+
+                if (i == readPosition) {
+                    context2.drawImage(this.read, 38 + 68*columnPosition, 38 + 68*rowPosition);
+                }
+
+                if (i > readPosition) {
+                    if (middlePosition < endPosition) {
+                        if (i <= middlePosition) {
+                            context2.drawImage(this.middle, 38 + 68*columnPosition, 38 + 68*rowPosition);
+                        } else {
+                            context2.drawImage(this.end, 38 + 68*columnPosition, 38 + 68*rowPosition);
+                        }
+                    } else {
+                        if (i <= endPosition) {
+                            context2.drawImage(this.end, 38 + 68*columnPosition, 38 + 68*rowPosition);
+                        } else {
+                            context2.drawImage(this.middle, 38 + 68*columnPosition, 38 + 68*rowPosition);
+                        }                    
+                    }                    
+                }
+
+                if (letterIndex < length-1) {
+                    letterIndex++;
+                    letterPosition = this.state.identiconLetters[letterIndex].charCodeAt()-'A'.charCodeAt();
+                }
+
+
+            } else {
+                if (i < writePosition) {
+                    context2.drawImage(this.optimized, 38 + 68*columnPosition, 38 + 68*rowPosition);                    
+                }
+
+                if (writePosition < i && i < readPosition) {
+
+                }
+
+                if (i > readPosition) {
+                    context2.drawImage(this.beginning, 38 + 68*columnPosition, 38 + 68*rowPosition);                       
+                }
+
             }
+                    
         }
     }
 
